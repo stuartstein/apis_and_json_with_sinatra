fb81e586f783887eb4e4a5b54e3005daf1a3fecb
stuartstein <stuart.stein@gmail.com>
2014-01-15 18:43:22 -0800

    Completed the sort

--- omdb.rb
+++ omdb.rb
@@ -28,12 +28,14 @@ post '/result' do
   search_str = params[:movie]
   
   # Make a request to the omdb api here!
-  result = imdb_pull(:s, search_str)
-  
+  movies = imdb_pull(:s, search_str)["Search"]
+  movies = movies.sort_by {|movie| movie["Year"].to_i}
+  movies.reverse! 
+
   # Modify the html output so that a list of movies is provided.
   html_str = "<html><head><title>Movie Search Results</title></head><body><h1>Movie Results</h1>\n<ul>"
 
-  result["Search"].each do |movie|
+  movies.each do |movie|
     html_str += "<li><a href = \"../poster/#{movie["imdbID"]}\">#{movie["Title"]} - #{movie["Year"]}</a></li>"
   end
     html_str += "</ul></body></html>"
@@ -41,10 +43,10 @@ end
 
 get '/poster/:imdb' do |imdb_id|
   
-  result = imdb_pull(:i, imdb_id)
+  movie = imdb_pull(:i, imdb_id)
   html_str = "<html><head><title>Movie Poster</title></head><body><h1>Movie Poster</h1>\n"
-  html_str += "<h3>#{result["Title"]}</h3>"
-  html_str += "<img src=\" #{result["Poster"]} \"><br />"
+  html_str += "<h3>#{movie["Title"]}</h3>"
+  html_str += "<img src=\" #{movie["Poster"]} \"><br />"
   html_str += '<br /><a href="/">New Search</a></body></html>'
 
 end
